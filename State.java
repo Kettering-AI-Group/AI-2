059eac4d2f31f61463ea03e292fb2e14784345ea
Ivan <Ivan>
2016-02-11 21:33:57 -0500

    Added in all need commenting for the style guideline

--- State.java
+++ State.java
@@ -12,23 +12,45 @@ public class State{
    String id = "";
    private int size;
    LinkedList<Integer> board = new LinkedList<Integer>();
-   
+
+   /**************************************************
+   Method Name: State
+   Input to the method: int[], the order and number of game pecices
+   Output(Return value): State, the configured State given inputs
+   Brief description of the task: Generates the "board" and init state of the game
+   Author:
+   **************************************************/
    public State(int[] values){
       for(int i: values){ 
          board.add(i); 
       } 
+      
       normalize();
       size = values.length;
       id = genHashId();
    }
-   
+
+   /**************************************************
+   Method Name: normalize
+   Input to the method: Void
+   Output(Return value): Void, reorders the virtual game board
+   Brief description of the task: Shifts all game parts until 1 is the leading unit
+   Author:
+   **************************************************/
    private void normalize(){
       while(board.peek() != 1){
          int temp = board.pop();
          board.addLast(temp);
       }
    }
-   
+
+   /**************************************************
+   Method Name: genHashId
+   Input to the method: Void
+   Output(Return value): String, the id based off of the order of the peices
+   Brief description of the task: This gives a unique id based on the order of the peices
+   Author:
+   **************************************************/
    private String genHashId(){
       int size = board.size();
       String hashId = "";
@@ -39,7 +61,14 @@ public class State{
       
       return hashId;
    }
-   
+
+   /**************************************************
+   Method Name: rotate
+   Input to the method: int, the first peices to start the rotation with
+   Output(Return value): Void, re-orders the peices
+   Brief description of the task: Gives the rotation function of the board game
+   Author:
+   **************************************************/
    public void rotate(int start){
       if(start < 1 || start > board.size()){
         throw new java.lang.IndexOutOfBoundsException();
@@ -61,7 +90,14 @@ public class State{
       normalize();
       id = genHashId();
    }
-   
+
+   /**************************************************
+   Method Name: printState
+   Input to the method: Void
+   Output(Return value): Void, outputs to the console
+   Brief description of the task: This gives outputs information based on state
+   Author:
+   **************************************************/
    public void printState(){
       int last = board.peekLast();
       int temp;
@@ -75,7 +111,14 @@ public class State{
       board.addLast(board.pop());
       System.out.println(last);
    }
-   
+
+   /**************************************************
+   Method Name: clone
+   Input to the method: Void
+   Output(Return value): State, a true clone of the array
+   Brief description of the task: This makes a deep clone of the state
+   Author:
+   **************************************************/
    public State clone(){
       int size = board.size();
       int[] array = new int[size];
@@ -87,11 +130,25 @@ public class State{
       State clone = new State(array);
       return clone; 
    }
-   
+
+   /**************************************************
+   Method Name: getId
+   Input to the method: Void
+   Output(Return value): String, gets a id base on state order
+   Brief description of the task: Gets the most current ID of the state based on state order
+   Author:
+   **************************************************/
    public String getId(){
       return genHashId();
    }
-   
+
+   /**************************************************
+   Method Name: size
+   Input to the method: Void
+   Output(Return value): int, returns number of elements in state
+   Brief description of the task: this gives the size of the state
+   Author:
+   **************************************************/
    public int size(){
       return size;
    }
